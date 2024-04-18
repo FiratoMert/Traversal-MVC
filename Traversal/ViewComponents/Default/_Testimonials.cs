43799b4ac051578e798105c801fbe4d80b78557f
FiratoMert <firatomert@gmail.com>
2024-04-18 09:26:42 +0300

    .

---
+++ Traversal/ViewComponents/Default/_Testimonials.cs
@@ -0,0 +1,17 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Traversal.ViewComponents.Default
+{
+    public class _Testimonials : ViewComponent
+    {
+        TestimonialsManager testimonialsManager = new TestimonialsManager(new EfTestimonialDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = testimonialsManager.TGetList();
+            return View(values);
+        }
+    }
+}
